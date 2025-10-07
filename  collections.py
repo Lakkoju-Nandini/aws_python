fb0da7fb37af4f083a50292f145f0ad52dcd6a41
EC2 Default User <ec2-user@ip-10-16-10-175.us-west-2.compute.internal>
2025-10-07 10:08:19 +0000

    first commit

---
+++  collections.py
@@ -0,0 +1,24 @@
+myFruitlist = ["Apple", "Banana", "Cherry"]
+print(myFruitlist)
+print(type(myFruitlist))
+print(myFruitlist[0])
+print(myFruitlist[1])
+print(myFruitlist[2])
+myFruitlist[2] = "Orange"
+print(myFruitlist)
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
